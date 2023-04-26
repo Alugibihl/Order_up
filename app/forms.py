8c168bb8987a8fc09d6deaa58fa71d6702679a76
alexl <alexandermlugbiihl@gmail.com>
2023-04-26 18:07:21 -0400

    login implement

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 
-class loginForm(FlaskForm):
+class LoginForm(FlaskForm):
     employee_number = StringField("Employee number", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Login")
