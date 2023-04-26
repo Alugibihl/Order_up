acd96354b60d6e25f7a7629c70428fb8a8afc167
alexl <alexandermlugbiihl@gmail.com>
2023-04-26 19:30:47 -0400

    data base update

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin  # New import
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 db = SQLAlchemy()
 
 
@@ -12,7 +13,7 @@ class Employee(db.Model, UserMixin):  # Your class definition
     name = db.Column(db.String(100), nullable=False)
     employee_number = db.Column(db.Integer, nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
-    
+
     @property
     def password(self):
         return self.hashed_password
@@ -23,3 +24,41 @@ class Employee(db.Model, UserMixin):  # Your class definition
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+class Menu(db.Model):
+    __tablename__ = "menus"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+
+    items = db.relationship(
+        "MenuItem",
+        back_populates="menu"
+    )
+
+
+class MenuItem(db.Model):
+    __tablename__ = "menu_items"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(db.Integer, db.ForeignKey("menus.id"), nullable=False)
+    menu_type_id = db.Column(db.Integer, db.ForeignKey("menu_item_types.id"), nullable=False)
+
+    menu = db.relationship(
+        "Menu",
+        back_populates="items"
+    )
+    type = db.relationship(
+        "MenuItemType",
+        back_populates="menu_item"
+    )
+
+
+class MenuItemType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+
+    menu_item = db.relationship(
+        "MenuItem",
+        back_populates="type"
+    )
